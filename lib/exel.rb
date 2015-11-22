2b2708abb45f26fe8fc142f8c19152ff5d64ab25
David <david@retailcommon.com>
2015-11-22 16:14:58 -0500

    Require all files when gem is loaded

--- lib/exel.rb
+++ lib/exel.rb
@@ -17,4 +17,7 @@ module EXEL
   def self.configure
     yield configuration
   end
+
+  root = File.expand_path('../..', __FILE__)
+  Dir[File.join(root, 'lib/exel/**/*.rb')].each { |file| require file }
 end
