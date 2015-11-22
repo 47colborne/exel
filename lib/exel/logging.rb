693763400bcf20380330cab5308c9c0191ed533e
David <david@retailcommon.com>
2015-11-22 17:49:25 -0500

    Initialize default logger if not set and allow level/filename to be set in configuration

--- lib/exel/logging.rb
+++ lib/exel/logging.rb
@@ -2,8 +2,25 @@ require 'logger'
 
 module EXEL
   module Logging
+    DEFAULT_LEVEL = :info
+
     def self.logger
+      @logger || initialize_logger
+    end
+
+    def self.initialize_logger
+      @logger = Logger.new(log_filename)
+      @logger.level = log_level
       @logger
+    end
+
+    def self.log_filename
+      EXEL.configuration[:log_filename] || '/dev/null'
+    end
+
+    def self.log_level
+      level = EXEL.configuration[:log_level] || DEFAULT_LEVEL
+      Logger.const_get(level.to_s.upcase)
     end
 
     def self.logger=(logger)
---
+++ spec/exel/logging_spec.rb
@@ -0,0 +1,36 @@
+module EXEL
+  describe Logging do
+    before { @restore_logger = Logging.logger }
+    after { Logging.logger = @restore_logger }
+
+    describe '.logger=' do
+      it 'should set a logger' do
+        logger = double(:logger)
+        Logging.logger = logger
+        expect(Logging.logger).to be(logger)
+      end
+
+      it 'should set a null logger when nil given' do
+        expect(Logger).to receive(:new).with('/dev/null')
+        Logging.logger = nil
+      end
+    end
+
+    describe '.logger' do
+      before { Logging.instance_variable_set(:@logger, nil) }
+
+      it 'should initialize the logger on first read if not already set' do
+        EXEL.configure do |config|
+          config[:log_level] = :warn
+          config[:log_filename] = 'log.txt'
+        end
+
+        logger = instance_double(Logger)
+        expect(Logger).to receive(:new).with('log.txt').and_return(logger)
+        expect(logger).to receive(:level=).with(Logger::WARN)
+
+        Logging.logger
+      end
+    end
+  end
+end
