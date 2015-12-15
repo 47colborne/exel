55896d166098ce926d9c1fa50b832ddfca47f557
David <david@retailcommon.com>
2015-12-15 10:44:49 -0500

    Removed S3Handler

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,10 +2,6 @@ Dir[File.expand_path('../../lib/**/*.rb', __FILE__)].each { |f| require f }
 
 EXEL.logger = nil
 
-EXEL.configure do |config|
-  config.aws = OpenStruct.new
-end
-
 module EXEL
   module Providers
     class DummyAsyncProvider
